99e27136f9495cf9f3da24be1646977711af581e
arshedgithub <arshedahmedmail@gmail.com>
2023-12-26 18:23:55 +0530

    route for get All usrs

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -89,4 +89,14 @@ router.post('/forgotPassword', (req, res) => {
     })
 })
 
+router.get('/get', (req, res) => {
+    var query = "Select id, name, email, contactNumber, status from user where role='user'";
+    connection.query(query, (err, results) => {
+        if (!err) {
+            return res.status(200).json(results);
+        } else {
+            return res.status(500).json(err)
+        }
+    })
+})
 module.exports = router;

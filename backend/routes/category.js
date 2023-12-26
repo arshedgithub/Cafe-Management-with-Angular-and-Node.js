9972d4785f891d01127118c7c1ed02059e8ede17
arshedgithub <arshedahmedmail@gmail.com>
2023-12-26 23:33:24 +0530

    new category route

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -6,3 +6,15 @@ require('dotenv').config();
 
 const auth = require('../services/auth')
 const admin = require('../services/admin')
+
+router.post('/add', auth.authenticateToken, admin.checkRole, (req, res, next) => {
+    const category = req.body;
+    var query = "insert into category(name) values(?)";
+    connection.query(query, [category.name], (err, results) => {
+        if (!err) {
+            return res.status(200).json({message: "category added successfully."})
+        } else {
+            return res.status(500).json(err); 
+        }
+    });
+})

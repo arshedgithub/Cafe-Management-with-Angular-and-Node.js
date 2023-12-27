c31b5b3f2b8ed1735c425e83ab9ff96d94c5a299
arshedgithub <arshedahmedmail@gmail.com>
2023-12-27 13:44:38 +0530

    view and deleting bill routes

--- backend/routes/bill.js
+++ backend/routes/bill.js
@@ -2,15 +2,17 @@ const express = require('express');
 const connection = require('../connection');
 const router = express.Router();
 
+const fs = require('fs');
 let ejs = require('ejs');
 let pdf = require('html-pdf');
 let path = require('path');
 let uuid = require('uuid');
 const { authenticateToken } = require('../services/auth');
+const { checkRole } = require('../services/admin');
 
 router.post('/generateReport', authenticateToken, (req, res) => {
     const generatedUuid = uuid.v1();
-    
+
     // get orderDetails by destructuring
     const {name, email, contactNumber, paymentMethod, totalAmount, productDetails } = req.body;
     const productDetailsReport = JSON.parse(productDetails);
@@ -22,7 +24,7 @@ router.post('/generateReport', authenticateToken, (req, res) => {
                 if (err) {
                     return res.status(500).json(err);
                 } else {
-                    pdf.create(data).toFile('./generate_pdf/'+ generatedUuid+'.pdf', (err, data) => {
+                    pdf.create(data).toFile('./generated_pdf/'+ generatedUuid+'.pdf', (err, data) => {
                         if (err) {
                             console.log(err);
                             return res.status(500).json(err);
@@ -37,5 +39,59 @@ router.post('/generateReport', authenticateToken, (req, res) => {
         }
     })
 });
+
+router.post('/getPdf', authenticateToken, (req, res) => {
+    const { uuid, productDetails, name, email, contactNumber, paymentMethod, totalAmount } = req.body;
+    const pdfPath = `./generated_pdf/${uuid}.pdf`;
+
+    if (fs.existsSync(pdfPath)) {
+        res.contentType('application/pdf');
+        fs.createReadStream(pdfPath).pipe(res);
+    } else {
+        var productDetailsReport = JSON.parse(productDetails);
+        ejs.renderFile(path.join(__dirname,'', 'report.ejs'), {productDetails: productDetailsReport, name, email, contactNumber, paymentMethod, totalAmount }, (err, data) => {
+            if (err) {
+                return res.status(500).json(err);
+            } else {
+                pdf.create(data).toFile('./generate_pdf/'+ uuid +'.pdf', (err, data) => {
+                    if (err) {
+                        console.log('err');
+                        return res.status(500).json(err);
+                    } else {
+                        res.contentType('application/pdf');
+                        fs.createReadStream(pdfPath).pipe(res);
+                    }
+                });
+            }
+        });
+
+    }
+});
+
+router.get('/getBills', authenticateToken, (req, res) => {
+    var query = "Select * from bill";
+    connection.query(query, (err, results) => {
+        if (!err) {
+            return res.status(200).json(results);
+        } else {
+            return res.status(500).json(err)
+        }
+    });
+});
+
+router.delete('/delete/:id', authenticateToken, (req, res) => {
+    const id = req.params.id;
+    const query = "Delete from bill where id=?";
+    connection.query(query, [id], (err, results) => {
+        if (!err) {
+            if (results.affectedRows == 0) {
+                return res.status(404).json("Bill id does not found");
+            }
+            return res.status(200).json("bill deleted successfully.");
+        } else {
+            return res.status(500).json(err)
+        }
+    });
+})
 
 module.exports = router;
